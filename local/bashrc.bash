eb0d8a5a863b36fc0a708d04b94f2ee4df08e60e
Anand Raghuraman <anandr@gmail.com>
2014-03-30 19:34:35 -0700

    bash_it:  - set iterm preferences  - add goto and echoe functions

--- local/bashrc.bash
+++ local/bashrc.bash
@@ -1,4 +1,16 @@
 #!/bin/bash
 # anand's customizations
-export PATH=$PATH:$HOME/.ccm/repository/2.0.1/bin
+export PATH=$PATH:$HOME/.ccm/repository/2.0.1/bin:${HOME}/src/github.com/twitter/scalding/scripts
+
+# Echoes to stderr
+#  usage: echoe hello there
+echoe() { echo "$@" 1>&2; }
+
+# Finds the first directory matching the given directory name pattern (gfind)
+#  usage: goto scalding
+function goto() {
+    local d=$(find ~/src/ -type d -name $1 -print -quit)
+    [[ -z ${d} ]] && echoe $1 not found && return 1
+    cd ${d}
+}
 
